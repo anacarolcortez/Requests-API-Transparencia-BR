a7f9d51416eb0c194ccbc71b39b2cd0daeadeca6
Ana Cortez <anacarolcortez@hotmail.com>
2022-08-29 13:26:41 -0300

    Consulta dados cartão corporativo PR 2019-22

--- get_cartao_corporativo.py
+++ get_cartao_corporativo.py
@@ -1,44 +1,44 @@
-# Documentação da API oferecida pelo portal Transparência Brasil: https://api.portaldatransparencia.gov.br/swagger-ui.html
+# Documentação Requests: https://requests.readthedocs.io/en/latest/user/quickstart/
+# Swagger Transparência Brasil: https://api.portaldatransparencia.gov.br/swagger-ui.html
 
 import requests
-import key
-import pandas as pd
 import json
+import chave
 from datetime import datetime, timedelta
 
-link = "http://api.portaldatransparencia.gov.br/api-de-dados/cartoes"
 
-s = requests.Session()
-s.headers.update({'chave-api-dados': key.value})
+def dia_atual():
+    hoje = datetime.now()
+    return hoje.strftime("%d/%m/%Y")
 
-
-def get_date_today():
-    today = datetime.now()
-    return today.strftime("%d/%m/%Y")
-
-
-# Se você quiser usar uma função que facilite consulta em um intervalo de dias;
-# ex: últimos 30, 60, 90 dias
-def get_date(days_to_subtract):
-    last_day = datetime.now()
-    return last_day - timedelta(days=days_to_subtract)
+# Se você quiser usar uma funcao que facilite a consulta em um intervalo de dias:
+def gera_data(variacao_dias):
+    hoje = datetime.now()
+    return hoje-timedelta(days=variacao_dias)
 
 
-def get_dados(paginas):
-    data = []
-    today = get_date_today()
+def consulta_cartoes(sessao, api, paginas, data_inicial, data_final):
+    lista_resultados = []
+    parametros = {
+        'codigoOrgao': '20101',
+        'dataTransacaoInicio': data_inicial,
+        'dataTransacaoFim': data_final,
+        'pagina': 1
+    }
+    for pg in range(1, paginas+1):
+        parametros['pagina'] = pg
+        resposta = sessao.get(api, params=parametros)
+        if resposta.status_code == 200:
+            dados_pg = json.loads(resposta.content)
+            for dado in dados_pg:
+                lista_resultados.append(dado)
+        else:
+            print(f"Erro na requisição, pg {pg}")
+    print(f"Total de resultados: {len(lista_resultados)}")
+    return lista_resultados
 
-    for i in range(1, paginas):
-        payload = {'codigoOrgao': 20101, 'dataTransacaoInicio': '01/01/2019',
-                   'dataTransacaoFim': today, 'pagina': i}
-        response = s.get(link, params=payload)
-        pgdata = json.loads(response.content)
-        data.append(pgdata[0])
 
-    return data
-
-
-def save_dataframe(lista_dicionarios):
+def salvar_dataframe(lista_dicionarios):
     df = pd.json_normalize(lista_dicionarios)
     # print(df.columns)
     labels = [
@@ -55,8 +55,14 @@ def save_dataframe(lista_dicionarios):
     df.to_csv('dados/cartao_corporativo', sep=';',
               encoding='utf-8', index=False)
     df.head(10)
+    
+    
+def requisicao_api_transparencia():
+    api = "http://api.portaldatransparencia.gov.br/api-de-dados/cartoes"
+    sessao = requests.Session()
+    sessao.headers.update({'chave-api-dados': chave.valor})
+    hoje = dia_atual()
+    dados = consulta_cartoes(sessao, api, 202, "01/01/2019", hoje)
+    salvar_dataframe(dados)
 
-
-# Infelizmente, a única forma de descobrir a quantidade de páginas é consultando a API (swagger ou postman/insomnia)
-card_expenses = get_dados(196)
-save_dataframe(card_expenses)
+# Infelizmente, a única forma de descobrir a quantidade de páginas é consultando a API (usar "try" no swagger ou o postman/insomnia)
